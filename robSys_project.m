f8486f482787815a70adb018e936b6d2600334e4
lacergunn <47068554+lacergunn@users.noreply.github.com>
2024-04-19 08:43:26 -0700

    Basic code for project, no functions yet

---
+++ robSys_project.m
@@ -0,0 +1,8 @@
+arm1base=[2,2,2]; %x y z coordinates of arm 1 starting position
+arm2base=[3,3,3]; %x y z coordinates of arm 2 starting position
+target1=[1,2,3]; %x y z coordinates of object 1 position
+target2=[4,5,6]; %x y z coordinates of object 2 position
+upperArmL=1.25; %upper arm length
+forearmL=1.25; %lower arm length. Same for both arms.
+
+
